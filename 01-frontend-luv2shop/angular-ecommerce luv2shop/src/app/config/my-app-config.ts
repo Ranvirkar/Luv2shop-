c4d9f1b25e22bf31e4532cd9b9bfd57aaf7b514b
Ranvirkar <ranvirkarvenkatesh2@gmail.com>
2022-07-20 16:39:55 +0530

    first commit

---
+++ 01-frontend-luv2shop/angular-ecommerce luv2shop/src/app/config/my-app-config.ts
@@ -0,0 +1,8 @@
+export default {
+    oidc: {
+        clientId: '0oa3qhc0c43V48N7m5d7',
+        issuer: 'https://dev-03346807.okta.com/oauth2/default',
+        redirectUri: 'https://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+}
